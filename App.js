6f31c3207a1bb35fc5e76e0a76d152fced9d912c
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-14 23:17:53 -0700

    Commited

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-function HomeScreen() {
+const App = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View>
+      <Text>App</Text>
     </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    
-  );
+  )
 }
 
 export default App;
+
+const styles = StyleSheet.create({});
