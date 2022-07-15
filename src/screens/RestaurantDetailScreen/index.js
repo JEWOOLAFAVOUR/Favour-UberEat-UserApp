aea08aaf03205fa4d705fe40fd3484fe6b10a38f
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-15 03:33:23 -0700

    Goodu

--- src/screens/RestaurantDetailScreen/index.js
+++ src/screens/RestaurantDetailScreen/index.js
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import restaurants from "../../../assets/data/restaurants.json"
@@ -7,10 +8,15 @@ const RestautantDetailPage = () => {
   return (
     <View style={styles.page}>
         <Image source={{uri: restaurant.image}} style={styles.image}/>
+    <View style={styles.iconContainer}>
+        <Ionicons name="arrow-back-circle" size={30} color="white" style={styles.imageIcon}/>
+    </View>
+    <View style={styles.container}>
       <Text style={styles.title}>{restaurant.name}</Text>
       <Text style={styles.subTitle}>
         $ ${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
         {restaurant.maxDeliveryTime} minutes</Text>    
+    </View>
     </View>
   )
 }
@@ -20,10 +26,26 @@ export default RestautantDetailPage;
 const styles = StyleSheet.create({
     page:{
         flex: 1,
-        padding: 10,
     },
     image:{
         width: '100%',
         aspectRatio: 4 / 3,
+    },
+    container:{
+        margin: 10,
+    },
+    iconContainer:{
+        position: 'absolute',
+        top: 50,
+        left: 20,
+    },
+    title:{
+        fontSize: 35,
+        fontWeight: '600',
+        marginVertical: 10,
+    },
+    subTitle:{
+        color: '#525252',
+        fontSize: 15,
     },
 });
