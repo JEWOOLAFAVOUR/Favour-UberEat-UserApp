fc12c98851d0c7f87e53e8e55bb525d0160614cd
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2022-07-15 05:26:37 -0700

    Nice

--- src/screens/DishDetailScreen/index.js
+++ src/screens/DishDetailScreen/index.js
@@ -1,14 +1,85 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import restaurant from '../../../assets/data/restaurants.json';
+import { AntDesign } from '@expo/vector-icons';
+
+const dish = restaurant[0].dishes[0];
 
 const DishDetailsScreen = () => {
+    const [quantity, setQuantity] = useState(1);
+
+    const onMinus = () => {
+        if (quantity > 1) {
+          setQuantity(quantity - 1)  
+        };
+    }
+    const onPlus = () => {
+        setQuantity(quantity + 1)
+    }
+
+    const getTotal = () => {
+        return (dish.price * quantity).toFixed(2);
+    }
   return (
-    <View>
-      <Text>Dish Detail</Text>
+    <View style={styles.page}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <View style={styles.separator} />
+      <View style={styles.row}>
+        <AntDesign name='minuscircleo' size={60} color={'black'} 
+            onPress={onMinus}/>
+        <Text style={styles.quantity}>{quantity}</Text>
+        <AntDesign name='pluscircleo' size={60} color={'black'}
+            onPress={onPlus}/>
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.btnText}>Add {quantity} item to basket($ {getTotal()})</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default DishDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        width: '100%',
+        paddingVertical: 40,
+        padding: 10,
+    },
+    name:{
+        fontSize: 30,
+        fontWeight: '600',
+        marginVertical: 10,
+    },
+    description:{
+        color:'grey',
+    },
+    separator:{
+        height: 1,
+        backgroundColor: 'lightgrey',
+        marginVertical: 10,
+    },
+    row:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',marginTop: 50,
+    },
+    quantity:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginHorizontal: 20,
+    },
+    button:{
+        backgroundColor: 'black',
+        marginTop: 'auto',
+        padding: 20,
+        alignItems: 'center',
+    },
+    btnText:{
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 20,
+    },
+})
